e80645b3b3a1d8168eee9a3d321acd4b95d879e1
Adriano Mitre <adriano.mitre@gmail.com>
2011-01-16 23:10:53 -0200

    documented (comment) heredoc_unindent.rb

--- lib/heredoc_unindent.rb
+++ lib/heredoc_unindent.rb
@@ -1,7 +1,35 @@
+# This module, which is included in String, define the heredoc unindentation method
+# +heredoc_unindent+ and alias it as +unindent+.
+#
 module HeredocUnindent
 
   VERSION = '1.0.0'
-  
+ 
+  # Removes leading whitespace from Ruby heredocs. In fact, it removes leading
+  # whitespace from each line of a string, but only as much as the first line.
+  # 
+  # === USAGE:
+  # 
+  #   if true
+  #     puts <<-EOS.unindent
+  #       How wonderful it is
+  #         to be able
+  #       to unindent heredocs
+  #     EOS
+  #   end
+  # 
+  # produces
+  # 
+  #   How wonderful it is
+  #     to be able
+  #   to unindent heredocs
+  # 
+  # instead of
+  # 
+  #         How wonderful it is
+  #           to be able
+  #         to unindent heredocs 
+  #
   def heredoc_unindent
     first_margin = self[/^\s*/].size
     min_margin = self.scan(/^\s*/).map(&:size).min
@@ -16,7 +44,7 @@ module HeredocUnindent
   
 end
 
-class String
+class String # :nodoc:
   include HeredocUnindent
 end
 
