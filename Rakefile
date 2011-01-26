7bc814bce621aa4cdeb7ba4c303b8ed3e0382f69
Adriano Mitre <adriano.mitre@gmail.com>
2011-01-26 18:00:27 -0200

    refactored mod into CoreExt::String, moved version info to Rakefile & removed irrelevant comments

--- Rakefile
+++ Rakefile
@@ -3,22 +3,10 @@
 require 'rubygems'
 require 'hoe'
 
-# Hoe.plugin :compiler
-# Hoe.plugin :cucumberfeatures
-# Hoe.plugin :gem_prelude_sucks
-# Hoe.plugin :inline
-# Hoe.plugin :manifest
-# Hoe.plugin :newgem
-# Hoe.plugin :racc
-# Hoe.plugin :rubyforge
-# Hoe.plugin :website
-
 Hoe.spec 'heredoc_unindent' do
-  # HEY! If you fill these out in ~/.hoe_template/Rakefile.erb then
-  # you'll never have to touch them again!
-  # (delete this comment too, of course)
-
   developer('Adriano Mitre', 'adriano.mitre@gmail.com')
+  
+  self.version = '1.0.1'
   
   self.readme_file = 'README.rdoc'
   self.history_file = 'History.rdoc'
@@ -32,4 +20,3 @@ end
 task :tests => [:test] do
   # aliasing :test with :tests for RVM ('rvm tests')
 end
-
--- lib/heredoc_unindent.rb
+++ lib/heredoc_unindent.rb
@@ -1,42 +1,41 @@
-# This module, which is included in String, define the heredoc unindentation method
-# +heredoc_unindent+ and alias it as +unindent+.
-#
-module HeredocUnindent
-
-  VERSION = '1.0.0'
- 
-  # Removes common margin from indented strings such as the ones produced by
-  # heredocs, i.e., strips out leading whitespace chars at the beggining of
-  # each line (but only as much as the line with the smallest margin).
-  #
-  # Unless the optional argument +warn_first_dif_min+ is set to false or nil, a
-  # warning is produced when the margin of the first line differs from the minimum.
-  #
-  def heredoc_unindent(warn_first_dif_min=true)
-    min_margin = self.scan(/^\s*/).map(&:size).min
-    if warn_first_dif_min
-      first_margin = self[/^\s*/].size
-      if first_margin != min_margin
-        puts "warning: margin of the first line differs from minimum margin"
+module CoreExt
+  module String
+    module HeredocUnindent
+    #
+    # This module, which is included in String, define the heredoc unindentation method
+    # +heredoc_unindent+ and alias it as +unindent+.
+    ##
+     
+      # Removes common margin from indented strings such as the ones produced by
+      # heredocs, i.e., strips out leading whitespace chars at the beggining of
+      # each line (but only as much as the line with the smallest margin).
+      #
+      # Unless the optional argument +warn_first_dif_min+ is set to false or nil, a
+      # warning is produced when the margin of the first line differs from the minimum.
+      #
+      def heredoc_unindent(warn_first_dif_min=true)
+        min_margin = self.scan(/^\s*/).map(&:size).min
+        if warn_first_dif_min
+          first_margin = self[/^\s*/].size
+          if first_margin != min_margin
+            puts "warning: margin of the first line differs from minimum margin"
+          end
+        end
+        re = Regexp.new('^\s{0,' + min_margin.to_s + '}'  ) # omitting the lower limit produces warnings and wrong behavior in ruby-1.8.7-p330 and ree-1.8.7-2010.02
+        self.gsub(re, '')
       end
+      alias unindent heredoc_unindent
+
+      # Performs HeredocUnindent#heredoc_unindent in place, returning self, or nil if no changes were made
+      #
+      def  heredoc_unindent!(warn_first_dif_min=true)
+        orig = self.dup
+        self.replace(self.heredoc_unindent(warn_first_dif_min))
+        self != orig ? self : nil
+      end
+      alias unindent! heredoc_unindent!
     end
-    re = Regexp.new('^\s{0,' + min_margin.to_s + '}'  ) # omitting the lower limit produces warnings and wrong behavior in ruby-1.8.7-p330 and ree-1.8.7-2010.02
-    self.gsub(re, '')
+    
+    ::String.class_eval { include ::CoreExt::String::HeredocUnindent }
   end
-  alias unindent heredoc_unindent
-
-  # Performs HeredocUnindent#heredoc_unindent in place, returning self, or nil if no changes were made
-  #
-  def  heredoc_unindent!(warn_first_dif_min=true)
-    orig = self.dup
-    self.replace(self.heredoc_unindent(warn_first_dif_min))
-    self != orig ? self : nil
-  end
-  alias unindent! heredoc_unindent!
-  
 end
-
-class String # :nodoc:
-  include HeredocUnindent
-end
-
